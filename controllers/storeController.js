8d0c5b13cce7700b0b68b7a072d446dee41a5a69
Ubuntu <ubuntu@ip-172-31-18-167.us-west-2.compute.internal>
2019-07-23 23:53:07 +0000

    Complete Querying Our Database For Stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,22 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
   res.render('index');
+}
+
+exports.addStore = (req, res) => {
+  res.render('editStore', { title: 'Add Store' });
+}
+
+exports.createStore = async (req, res) => {
+  const store = await (new Store(req.body)).save();
+  req.flash('success', `Successfully created ${store.name}`)
+  res.redirect(`/store/${store.slug}`);
+}
+
+exports.getStores = async (req, res) => {
+  // query database for list of stores
+  const stores = await Store.find();
+  res.render('stores', { title: 'Stores', stores });
 }
