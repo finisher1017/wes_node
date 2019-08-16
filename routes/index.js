f74d6a03a955bef06fdcf762e7563b156fc1f939
Ubuntu <ubuntu@ip-172-31-18-167.us-west-2.compute.internal>
2019-08-16 22:03:40 +0000

    Complete Saving Registered Users to the Database

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
+const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
-router.get('/', storeController.homePage);
+router.get('/', storeController.getStores);
 router.get('/stores', catchErrors(storeController.getStores));
 router.get('/add', storeController.addStore);
 router.get('/store/:id/edit', catchErrors(storeController.editStore));
@@ -23,6 +25,18 @@ router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 
 router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
+
+router.get('/login', userController.loginForm)
+router.get('/register', userController.registerForm);
+
+// 1. Validate the registration data
+// 2. Register the user
+// 3. Log them in
+router.post('/register', 
+  userController.validateRegister,
+  userController.register,
+  authController.login
+);
 
 
 module.exports = router;
