d1948277794df9e831a234fe1b295f4f80d06e61
Ubuntu <ubuntu@ip-172-31-18-167.us-west-2.compute.internal>
2019-08-17 21:15:34 +0000

    Complete Creating a User Account Edit Screen

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,3 +6,19 @@ exports.login = passport.authenticate('local', {
   successRedirect: '/',
   successFlash: 'You are now logged in!'
 });
+
+exports.logout = (req, res) => {
+  req.logout();
+  req.flash('success', 'Successfully logged out!');
+  res.redirect('/');
+}
+
+exports.isLoggedIn = (req, res, next) => {
+  // first check if the user is authenticated
+  if(req.isAuthenticated()) {
+    next(); // continue
+    return;
+  }
+  req.flash('error', 'You must be logged in to add a store.');
+  res.redirect('/login');
+}
